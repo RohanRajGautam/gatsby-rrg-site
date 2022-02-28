65ef61f0f3de77f63667329db00605a9f490b1f5
Rohan Gautam <rohan@webpoint.io>
2022-03-01 02:13:06 +0545

    Change title to resume

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -2,21 +2,28 @@ import React from "react"
 import Layout from "../components/Layout"
 import Title from "../components/Title"
 import SEO from "../components/SEO"
-import { withPrefix } from 'gatsby'
+import { withPrefix } from "gatsby"
 
 const Cv = () => {
-
   return (
     <Layout>
       <SEO title="CV" description="resume of rohan raj gautam" />
       <section className="cv-page projects">
         <article className="cv">
-            <Title title="Curriculum Vitae" />
-            <a download="rohan_cv.pdf" className="btn" href={withPrefix('/rohan_cv.pdf')} rel="noopener noreferrer">
-                Download
-            </a>
-            <iframe title="cv" style={{width: '100%', height: '800px', marginTop:'2rem'}} src={withPrefix('/rohan_cv.pdf')}>
-            </iframe>
+          <Title title="Résumé" />
+          <a
+            download="rohan_cv.pdf"
+            className="btn"
+            href={withPrefix("/rohan_cv.pdf")}
+            rel="noopener noreferrer"
+          >
+            Download
+          </a>
+          <iframe
+            title="cv"
+            style={{ width: "100%", height: "800px", marginTop: "2rem" }}
+            src={withPrefix("/rohan_cv.pdf")}
+          ></iframe>
         </article>
       </section>
     </Layout>
